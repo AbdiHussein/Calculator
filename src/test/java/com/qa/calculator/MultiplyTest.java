5999a6e0bece3db53a81f648afb211f3e0ab0770
AbdiHussein <abdihussein_93@hotmail.com>
2018-04-19 10:52:03 +0100

    Multiply feature using TDD

---
+++ src/test/java/com/qa/calculator/MultiplyTest.java
@@ -0,0 +1,19 @@
+package com.qa.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MultiplyTest {
+	
+	private Multiply multiply;
+	
+
+	@Test
+	public void test() {
+		multiply = new Multiply();
+		int expectedValue = 4;
+		int actualValue = multiply.multiply(2,2);
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+}
